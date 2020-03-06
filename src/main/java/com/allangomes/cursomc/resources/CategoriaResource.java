4dd7658354c4362f52acf17608f09e573986536c
Allan Gomes <traduvert@icloud.com>
2020-03-06 15:25:19 -0300

    o

--- src/main/java/com/allangomes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/allangomes/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,26 @@
 package com.allangomes.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.allangomes.cursomc.domain.Categoria;
 
 @RestController
 public class CategoriaResource {
 	
 	@GetMapping(value="/categorias")
-	public String listar() {
-		return "REST is working!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 	
 }
