0f3b83286a7bac969077d93519cf4279dd9c3aa9
Allan Gomes <traduvert@icloud.com>
2020-03-06 13:56:02 -0300

    Projeto criado

---
+++ src/test/java/com/allangomes/cursomc/Cursomc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allangomes.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
